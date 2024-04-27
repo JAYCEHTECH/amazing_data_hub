dc36fb637112338149407ca53812d95f5708eb0d
mikegyamfi <codeage20@gmail.com>
2024-04-27 19:47:07 +0000

    initial commit

--- intel/settings.py
+++ intel/settings.py
@@ -31,7 +31,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
-    'amazing-data-hub-8e677.ondigitalocean.app'
+    'amazing-data-hub-8e677.ondigitalocean.app',
+    'www.amazingdatahubgh.com'
 ]
 
 
